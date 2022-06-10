34c16249ba6948855898b0fb60f68057211f65b4
Lena Palasionak <19lenka87@tut.by>
2022-06-10 17:07:35 +0300

    2. AOP functionality added.

---
+++ src/main/java/com/lenapalasionak/spring/mvc/aspect/MyLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.lenapalasionak.spring.mvc.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.lenapalasionak.spring.mvc.dao.* .*(..))")
+    public Object aroundAllRepositoryMethodAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("End of " + methodName);
+        return targetMethodResult;
+
+    }
+
+}
